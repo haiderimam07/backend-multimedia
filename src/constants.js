0406b4a5f3324e0c16b12fa74a191721632034f6
haiderimam07 <haiderimam1207@gmail.com>
2025-09-30 19:21:00 +0530

    check commit

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 // in this file we are storing all the constants that we use locally in our app
 export const DB_NAME="multitube";
+export const DESC="Multitube platform that let the user watch and upload video"
